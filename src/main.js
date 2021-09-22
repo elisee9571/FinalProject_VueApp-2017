7f7952cccbe198f6ccdd62563cf84c8e40645a1b
elisee9571 <69207830+elisee9571@users.noreply.github.com>
2021-09-22 18:43:48 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
-// Vue.prototype.$apiurl = "https://api.2017.ovh/";
-Vue.prototype.$apiurl = "http://localhost:3000/";
+Vue.prototype.$apiurl = "https://api.2017.ovh/";
+//Vue.prototype.$apiurl = "http://localhost:3000/";
 
 /* import axios pour liée mon node et vue grace aux requetes*/
 /* Axios est une bibliothèque, Elle permet de communiquer avec des API en utilisant des requêtes. */
